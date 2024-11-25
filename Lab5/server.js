4da877438f6f8c483008c08944dc9e4e70f94e75
Amy Mcevoy <G00413220@atu.ie>
2024-11-25 14:53:51 +0000

    Fixed Errors + Tidied up code

--- Lab5/server.js
+++ Lab5/server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to Data Representation & Querying');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
 });
 
 //Error handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
+});
+
+//Starting server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
