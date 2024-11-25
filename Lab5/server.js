dc1e973074531b5f34b060dcc5b19975a87d0a66
Amy Mcevoy <G00413220@atu.ie>
2024-11-25 14:54:24 +0000

    First name as message

--- Lab5/server.js
+++ Lab5/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//message
 app.get('/hello/:name', (req, res) => {
     const name = req.params.name;
     res.send(`Hello ${name}`);
