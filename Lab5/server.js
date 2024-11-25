9395ca34a47c5fbec7508417db6767fb5c49c523
Amy Mcevoy <G00413220@atu.ie>
2024-11-25 18:10:20 +0000

    added movies route

--- Lab5/server.js
+++ Lab5/server.js
@@ -14,6 +14,33 @@ app.get('/hello/:name/:surname', (req,res) => {
     res.send(`Hello ${name}, ${surname}`);
 });
 
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.json({ movies });
+});
+
 //Error handling
 app.use((err, req, res, next) => {
     console.error(err.stack);
