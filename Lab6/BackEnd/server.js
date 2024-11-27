58e63b10e03b66a82044d38e85d8a67a82a64bd3
Amy Mcevoy <G00413220@atu.ie>
2024-11-27 11:39:19 +0000

    Backend created

---
+++ Lab6/BackEnd/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.get('/api/movies', (req, res) => {
+    const myMovies = {
+        "myMovies": [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ]};
+    res.status(201).json(myMovies); 
+});
+
+//Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+//Starting server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
